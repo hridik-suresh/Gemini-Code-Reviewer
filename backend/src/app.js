9fc4c0b5c9c1b6918003c46e41fc89c7fd8d0507
hridik-suresh <iamhridiksuresh@gmail.com>
2026-01-08 19:58:13 +0530

    Updated cors in backend. Project is completed.

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,11 @@ const aiRoutes = require("./routes/ai.routes");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://gemini-code-reviewer-two.vercel.app/",
+  })
+);
 
 app.use(express.json({ limit: "5mb" }));
 
